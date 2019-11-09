3edcfb79a4f2586be9e8004873151dc6bc0d95db
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2019-11-09 14:07:19 +0200

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,12 @@
+
+public class Card {
+	public int number;
+	public String type;
+	public Boolean hidden=false;
+	
+	public Card(int number,String type)
+	{
+		this.number=number;
+		this.type=type;
+	}
+}
